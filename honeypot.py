183c3034608f4da32081f868c2e18e35ac678c32
pmmp-tcc <108645049+pmmp-tcc@users.noreply.github.com>
2022-07-09 14:16:27 -0300

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -7,43 +7,43 @@ import schedule
 
 def honeypot(a=0, b=9, m=0, n=len(keys)):
     for c in range(m, n):
-        auth = tweepy.OAuthHandler(keys[c]['API_Key'], keys[c]['API_Key_Secret'])
+        auth = tweepy.OAuthHandler(keys[c]['API_Key'], keys[c]['API_Key_Secret']) #acessar as chaves das contas honeypots
         auth.set_access_token(keys[c]['Access_Token'], keys[c]['Access_Token_Secret'])
         api = tweepy.API(auth, wait_on_rate_limit=True)
 
         x = random.randint(a, b) #sortear número aleatório entre 0 e 9
         print(keys[c]['User'], x)
 
-        if 0 <= x < 4: #publica tweet, que é só texto
+        if 0 <= x < 4: #publica texto aleatório armazenado em uma lista
             text = open('arquivo.txt', 'r', errors="ignore") #abre o arquivo txt
             tweet = text.readlines()
             if tweet == []: #se a lista estiver vazia
-                print('vazio') #dá uma lista vazia
+                print('vazio') #retorna "vazio"
                 print(api.update_status('lista de tweets vazia')) #reabastecer a lista
             else:
                 print(api.update_status(tweet[0]))
                 del tweet[0]
-                text = open('arquivo.txt', 'w', errors="ignore")
-                text.writelines(tweet)
+                text = open('arquivo.txt', 'w', errors="ignore") #abrir arquivo.txt
+                text.writelines(tweet) #publicar texto do arquivo.txt
                 text.close()
 
         elif 4 <= x < 6:
             BRAZIL_WOE_ID = 23424768 #código do Brasil
-            trends = api.get_place_trends(BRAZIL_WOE_ID) #publica trend topics
+            trends = api.get_place_trends(BRAZIL_WOE_ID) #publica um dos assuntos do momento no Brasil
 
             topicos = list()
             for assunto in trends[0]["trends"]:
-                topicos.append(assunto["name"])
+                topicos.append(assunto["name"])  #PARA PUBLICAR O QUE??
 
-            text = open('arquivo.txt', 'r', errors="ignore")
-            tweet = text.readlines()
+            text = open('arquivo.txt', 'r', errors="ignore")  #PQ ABRIR ARQUIVO.TXT??
+            tweet = text.readlines()  #??
             if tweet == []:
-                print(random.choice(topicos[:3]) + 'trend vazia')
-                print(api.update_status(random.choice(topicos[:3]) + ' ' + 'lista de tweets vazia'))
+                print(random.choice(topicos[:3]) + 'trend vazia')  #??
+                print(api.update_status(random.choice(topicos[:3]) + ' ' + 'lista de tweets vazia'))  #??
             else:
-                print(api.update_status(random.choice(topicos[:3]) + ' ' + tweet[0]))
+                print(api.update_status(random.choice(topicos[:3]) + ' ' + tweet[0]))   #??
                 del tweet[0]
-                text = open('arquivo.txt', 'w', errors="ignore")
+                text = open('arquivo.txt', 'w', errors="ignore")  #??
                 text.writelines(tweet)
                 text.close()
 
