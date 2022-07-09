b8914b0297a24196cb35c7c549303aa9d6f02f94
pmmp-tcc <108645049+pmmp-tcc@users.noreply.github.com>
2022-07-09 16:47:06 -0300

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -7,7 +7,7 @@ import schedule
 
 def honeypot(a=0, b=9, m=0, n=len(keys)):
     for c in range(m, n):
-        auth = tweepy.OAuthHandler(keys[c]['API_Key'], keys[c]['API_Key_Secret']) #acessar as chaves das contas honeypots
+        auth = tweepy.OAuthHandler(keys[c]['API_Key'], keys[c]['API_Key_Secret']) #acessar a API através das chaves de cada conta honeypot
         auth.set_access_token(keys[c]['Access_Token'], keys[c]['Access_Token_Secret'])
         api = tweepy.API(auth, wait_on_rate_limit=True)
 
@@ -19,61 +19,61 @@ def honeypot(a=0, b=9, m=0, n=len(keys)):
             tweet = text.readlines()
             if tweet == []: #se a lista estiver vazia
                 print('vazio') #retorna "vazio"
-                print(api.update_status('lista de tweets vazia')) #reabastecer a lista
+                print(api.update_status('lista de tweets vazia')) #post no Twitter de aviso que a lista está vazia
             else:
-                print(api.update_status(tweet[0]))
-                del tweet[0]
-                text = open('arquivo.txt', 'w', errors="ignore") #abrir arquivo.txt
-                text.writelines(tweet) #publicar texto do arquivo.txt
+                print(api.update_status(tweet[0])) #publicar a primeira linha de texto do arquivo.txt
+                del tweet[0] #excluir a primeira linha do arquivo.txt
+                text = open('arquivo.txt', 'w', errors="ignore") #abrir arquivo.txt em modo de edição
+                text.writelines(tweet) #salva arquivo.txt sem a primeira linha
                 text.close()
 
         elif 4 <= x < 6:
             BRAZIL_WOE_ID = 23424768 #código do Brasil
-            trends = api.get_place_trends(BRAZIL_WOE_ID) #publica um dos assuntos do momento no Brasil
+            trends = api.get_place_trends(BRAZIL_WOE_ID) #buscar os assuntos do momento no Brasil
 
             topicos = list()
             for assunto in trends[0]["trends"]:
-                topicos.append(assunto["name"])  #PARA PUBLICAR O QUE??
+                topicos.append(assunto["name"])  #para colocar termos do assunto do momento em uma lista
 
-            text = open('arquivo.txt', 'r', errors="ignore")  #PQ ABRIR ARQUIVO.TXT??
-            tweet = text.readlines()  #??
-            if tweet == []:
-                print(random.choice(topicos[:3]) + 'trend vazia')  #??
-                print(api.update_status(random.choice(topicos[:3]) + ' ' + 'lista de tweets vazia'))  #??
+            text = open('arquivo.txt', 'r', errors="ignore")  #abrir o arquivo.txt (modo leitura apenas)
+            tweet = text.readlines()  #cada linha do arquivo.txt vira um elemento na lista tweet
+            if tweet == []: #se o arquivo.txt estiver vazio
+                #print(random.choice(topicos[:3]) + 'trend vazia')  #EXCLUIR ESSA LINHA
+                print(api.update_status('lista de tweets vazia'))  #post no Twitter de aviso que a lista está vazia
             else:
-                print(api.update_status(random.choice(topicos[:3]) + ' ' + tweet[0]))   #??
-                del tweet[0]
-                text = open('arquivo.txt', 'w', errors="ignore")  #??
-                text.writelines(tweet)
+                print(api.update_status(random.choice(topicos[:3]) + ' ' + tweet[0]))   #escolher 1 dos 3 primeiros tópicos presentes na lista topicos mais a primeira linha do arquivo.txt
+                del tweet[0] #excluir a primeira linha do tweet.txt
+                text = open('arquivo.txt', 'w', errors="ignore")  #abrir arquivo.txt em modo de edição
+                text.writelines(tweet) #salva arquivo.txt sem a primeira linha
                 text.close()
 
         elif 6 <= x < 8:
             BRAZIL_WOE_ID = 23424768
-            trends = api.get_place_trends(BRAZIL_WOE_ID)
+            trends = api.get_place_trends(BRAZIL_WOE_ID) #buscar os assuntos do momento no Brasil
 
             topicos = list()
             for assunto in trends[0]["trends"]:
-                topicos.append(assunto["url"]) #publicar links
+                topicos.append(assunto["url"]) #pega o url de cada assunto do momento 
 
-            print(api.update_status('Confira as tendências: ' + random.choice(topicos)))
+            print(api.update_status('Confira as tendências: ' + random.choice(topicos))) #publica o link
 
         elif 8 <= x < 10:
             contas = list()
             for user in keys:
-                contas.append(user['User'])
-            contas.pop(c)
-            usuario = random.choice(contas)
-            tweets = api.user_timeline(screen_name=usuario, count=10) 
-            ids = list()
+                contas.append(user['User']) #cria uma lista com as contas honeypots
+            contas.pop(c) #exclui da lista a conta que vai retweetar
+            usuario = random.choice(contas) #escolhe uma das 3 contas na lista de forma aleatória
+            tweets = api.user_timeline(screen_name=usuario, count=10) #busca os 10 últimos tweets da conta escolhida na linha 65
+            ids = list() 
             for tweet in tweets:
-                ids.append(tweet.id)
-            retweet = random.choice(ids)
+                ids.append(tweet.id) #pegando id dos 10 tweets (cada tweet tem um id)
+            retweet = random.choice(ids) #escolher aleatoriamente um tweet da lista ids
             try:
                 print(api.retweet(retweet)) #retweetar
-                print(api.create_favorite(retweet))
+                print(api.create_favorite(retweet)) #para dar like na publicação
 
             except tweepy.errors.Forbidden:
-                honeypot(8, 9, c, c + 1)
+                honeypot(8, 9, c, c + 1) #se tentar retweetar algo que já foi retweetado, dá erro. Tenta retweetar um novo tweet
 
         time.sleep(random.randint(0, 30)) #para dar intervalo de tempo entre um ciclo e outro
 
@@ -81,5 +81,5 @@ def honeypot(a=0, b=9, m=0, n=len(keys)):
 schedule.every(10).seconds.do(honeypot) #agendar para rodar a função honeypot a cada 10 segundos 
 while True:
     schedule.run_pending()
-    time.sleep(random.randint(2760, 4140))
+    time.sleep(random.randint(2760, 4140)) #para ter 20 a 25 publicações diárias
 
